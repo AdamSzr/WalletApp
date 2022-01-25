078a33ecde93b6ba2b0fa03feca393e0f89d33e6
AdamSzr <adam.szr98@gmail.com>
2022-01-25 17:41:43 +0100

    added migration to app docerfile

--- dbSetup/app/api/ping.ts
+++
@@ -1,8 +0,0 @@
-import { BlitzApiHandler, BlitzApiRequest, BlitzApiResponse, Middleware, useQuery } from "blitz"
-import db from "db"
-
-const ping: Middleware = (req, res) => {
-  res.status(200).json({ ping: "pong" })
-  res.end()
-}
-export default ping
